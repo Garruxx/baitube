ca16059106c261dd90bfa8bcd65ded9ab1759b40
Jhon (Garrux) <igarrux@gmail.com>
2024-08-08 07:18:29 -0500

    build(types): :label: global.d.ts is created for supporting .txt .svg and .gql

---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+declare module '*.gql' {
+	import { DocumentNode } from 'graphql'
+	const value: string
+	export default value
+}
+
+declare module '*.svg' {
+	const value: string
+	export default value
+}
+
+declare module '*.txt' {
+	const value: string
+	export default value
+}
