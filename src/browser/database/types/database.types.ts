562858500d4e7e70429ffacb8b7c15fb7cef6fb7
Jhon (Garrux) <igarrux@gmail.com>
2024-08-09 23:49:40 -0500

    fix(database/types): :label: database types errors fixed

--- src/browser/database/types/database.types.ts
+++ src/browser/database/types/database.types.ts
@@ -1,15 +1,15 @@
 import { type RemoveOptions, type UpdateOptions } from 'nedb'
 export type RemoveAsync = <T>(
 	query: T,
-	options: RemoveOptions
+	options?: RemoveOptions
 ) => Promise<number>
 export type InsertAsync = <T>(newDoc: any) => Promise<T>
 
 export type UpdateAsync = <T>(
 	query: T,
 	updateQuery: T,
-	options: UpdateOptions
+	options?: UpdateOptions
 ) => Promise<number>
 
-export type FindAsync = <T>(query: any, projection: any) => Promise<Array<T>>
-export type FindOneAsync = <T>(query: any, projection: any) => Promise<T>
+export type FindAsync = <T>(query: any, projection?: any) => Promise<Array<T>>
+export type FindOneAsync = <T>(query: any, projection?: any) => Promise<T>
