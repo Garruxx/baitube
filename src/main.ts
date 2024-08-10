484a45aa1c2432320cb815cd9c651ea08454e440
Jhon (Garrux) <igarrux@gmail.com>
2024-08-09 23:55:50 -0500

    feat(main): :sparkles: main use the new methods

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,24 @@
-import { join } from 'path'
-import {
-	useMultiFileAuthState,
-	makeWASocket,
-	type WASocket,
-} from '@whiskeysockets/baileys'
-import { GeneralSearch } from './youtube/general'
-import { DownloadVideo } from './downloader/download-video'
-import { DownloadSong } from './downloader/donwload-song'
-import { rm } from 'fs/promises'
-useMultiFileAuthState('tokens/garrux-s').then(({ state, saveCreds }) => {
-	const sock: WASocket = makeWASocket({
-		printQRInTerminal: true,
-		auth: state,
-	})
+import ytdl from '@distube/ytdl-core'
+import { logger } from './logger/logger'
+import { Whatsapp } from './whatsapp/whatsapp'
+import * as Baileys from '@whiskeysockets/baileys'
+import { Downloader } from './downloader/downloader'
+import { YTBrowser } from './browser/yt-browser'
+import { messageSimplifier } from './utils/message-simplifier'
+import { musicCard } from './browser/templates/music/music-card'
+import { config } from 'dotenv'
+config()
+if (!process.env.YT_KATZE_URL) throw new Error('env YT_KATZE_URL is required')
 
-	sock.logger.level = 'error'
-	sock.ev.on('creds.update', saveCreds)
-	sock.ev.on('messages.upsert', ({ messages }) => {
-		const ephemeralMessage = messages?.[0]?.message?.ephemeralMessage
-		const extendedText = messages?.[0]?.message?.extendedTextMessage
-		const quotedMessage = extendedText?.contextInfo?.quotedMessage
-		const key = messages[0]?.key
-		const quoted = quotedMessage?.imageMessage?.caption
-		let content = ephemeralMessage?.message?.extendedTextMessage?.text
-		content ??= extendedText?.text
-		content ??= messages[0].message?.conversation
-		const from = messages?.[0]?.key?.remoteJid
-		if (!from || !content) return
+const whatsapp = new Whatsapp('base', Baileys, logger as any)
+const downloader = new Downloader(whatsapp, ytdl, logger)
+new YTBrowser(
+	process.env.YT_KATZE_URL,
+	whatsapp,
+	messageSimplifier,
+	musicCard,
+	downloader.saveMessageSongData.bind(downloader),
+	logger
+)
 
-		GeneralSearch({ content, from }, sock, key)
-		DownloadVideo({ content, from }, sock, key, quoted!)
-		DownloadSong({ content, from }, sock, key, quoted!)
-	})
-})
-
-setInterval(() => {
-	rm(join(process.cwd(), 'temp'), {
-		recursive: true,
-		retryDelay: 20000,
-	})
-}, 1000 * 60 * 60 * 22)
+whatsapp.start()
