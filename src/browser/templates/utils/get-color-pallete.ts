412418eb72be4ce27925b3322588e5772acf1b48
Jhon (Garrux) <igarrux@gmail.com>
2024-08-11 03:26:08 -0500

    feat(templates/utils): :sparkles: get color pallete is updated for get better colors

--- src/browser/templates/utils/get-color-pallete.ts
+++ src/browser/templates/utils/get-color-pallete.ts
@@ -1,24 +1,29 @@
-import vibrant from 'node-vibrant'
+import sharp from 'sharp'
+import quantize, { type RgbPixel } from 'quantize'
+import { getContrastingColor } from './get-contrasting-color'
+import { splitArray } from './split-array'
+import { logger } from '../../../logger/logger'
+const base = {
+	bg: '#E6D8EE',
+	color: '#2E2B30',
+	title_color: '#C676F4',
+	vibrant: '#C676F4',
+}
 export const getColorPallete = async (img: Buffer | null) => {
-	const base = {
-		bg: '#E6D8EE',
-		color: '#2E2B30',
-		title_color: '#C676F4',
-		vibrant: '#C676F4',
-	}
 	if (!img) return base
 	try {
-		const pallete = await vibrant.from(img!).getPalette()
-
-		return {
-			bg: pallete.Vibrant?.hex || base.bg,
-			color: pallete.Vibrant?.bodyTextColor || base.color,
-			title_color: pallete.DarkMuted?.hex || base.title_color,
-			vibrant: pallete.Vibrant?.hex || base.vibrant,
-		}
+		const pxs = await sharp(img).raw().toBuffer({ resolveWithObject: true })
+		const rgbArray = splitArray(pxs.data, 3) as RgbPixel[]
+		const colorMap = quantize(rgbArray, 2)
+		if (!colorMap) return base
+		const pallete = colorMap.palette()
+		const color = `rgb(${getContrastingColor(pallete[0]).join(',')}`
+		const vibrant = `rgb(${getContrastingColor(pallete[1]).join(',')}`
+		const title_color = `rgb(${pallete[1].join(',')}`
+		const bg = `rgb(${pallete[0].join(',')}`
+		return { bg, color, title_color, vibrant }
 	} catch (error) {
-		console.log(error)
-
+		logger.error(error)
 		return base
 	}
 }
